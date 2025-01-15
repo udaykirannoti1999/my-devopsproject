52cedebeb2a64ac3773f69ad3dc099dbf13186f9
Ubuntu <ubuntu@ip-10-0-0-8.ap-south-1.compute.internal>
2025-01-15 11:19:31 +0000

    Describe your changes here, e.g., 'Fix bug in app.js'

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-    res.send("Hello, Dockerized Node.js! My name is");
+    res.send("Hello, Dockerized Node.js!");
 });
 
 const PORT = process.env.PORT || 3000;
