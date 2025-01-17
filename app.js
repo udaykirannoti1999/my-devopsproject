485d01a2d8f5e1b4172acf505deadb46e0030b23
Ubuntu <ubuntu@ip-10-0-0-8.ap-south-1.compute.internal>
2025-01-17 10:09:23 +0000

    Describe your changes here, e.g., 'Fix bug in app.js'

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-    res.send("Hello, Dockerized Node.js udaykiran is  a tester in yuma and yulu using java !");
+    res.send("Hello, Dockerized Node.js udaykiran is  a tester in yuma in golang !");
 });
 
 const PORT = process.env.PORT || 3000;
