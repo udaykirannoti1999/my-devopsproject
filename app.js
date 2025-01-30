caf3f9ff50bf036fa158e5bb761f98152d5e4ab4
udaykirannoti1999 <udaykirankittu00@gmail.com>
2025-01-30 16:27:31 +0000

    Describe your changes here, e.g., 'Fix bug in deploy.js'

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-    res.send("Hello, Dockerized Node.js udaykiran is  a tester in yuma in python and Gen ai !");
+    res.send("Hello, Dockerized Node.js udaykiran is  a tester in yuma in python and Gen ai and chat gpt\\\ !");
 });
 
 const PORT = process.env.PORT || 3000;
