d6f3c91418abeff756480b5d4f6f2e250b5bcf6c
udaykirannoti1999 <udaykirankittu00@gmail.com>
2025-02-04 18:03:54 +0000

    Describe your changes here, e.g., 'Fix bug in deploy.js'

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-    res.send("Hello, Dockerized Node.js udaykiran is  a tester in yuma in python and ai  deepseek!");
+    res.send("Hello, Dockerized Node.js udaykiran is  a tester in yuma in python and gemini!");
 });
 
 const PORT = process.env.PORT || 3000;
